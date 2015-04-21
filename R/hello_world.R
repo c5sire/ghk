90c437412acddd5e40da5f6b4bd03abbaa3514ba
Reinhard Simon <rsimon64@gmail.com>
2015-04-21 10:22:32 -0500

    added name parameter to hello_world, closes #2

--- R/hello_world.R
+++ R/hello_world.R
@@ -2,8 +2,9 @@
 #' 
 #' The typical hello world example.
 #' 
-#' 
+#' @param name a name for the greeting
+#' @return string a greeting
 #' @export
-hello_world <- function() {
-  "hello"
+hello_world <- function(name = "world") {
+  paste("Hello, ", name, "!", sep="")
 }
